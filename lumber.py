ca19eea88c13ff4d45630d6a13ed2a796e0e631c
thiagop-usp <thiagop@usp.br>
2019-03-03 18:53:45 -0300

    sometimes he does well, sometimes hes shit, idk, still something is wrong

--- lumber.py
+++ lumber.py
@@ -10,18 +10,15 @@ keyboard = Controller()
 
 num_branches = 6
 # from left to right in px
-branches_x = [121,121,121,121,121,121]
+branches_x = 121
 # from top to bottom in px
-branches_y = [458, 408, 359, 309, 259, 209]
+branches_y = [435, 385, 335, 285, 235, 186]
 
-wood_rgba = [64, 114, 167, 255] 
+wood_rgba = [64, 114, 167, 255]
 
-def is_tree(img_matrix, i):
-    x_pos = branches_x[i]
-    y_pos = branches_y[i]
-    spot = img_matrix[y_pos][x_pos]
-    print(str((y_pos, x_pos)) + " " + str(spot))
-    return spot[0] == wood_rgba[0] and spot[1] == wood_rgba[1] and spot[2] == wood_rgba[2]
+def is_tree(spot, i):
+    print(spot[i])
+    return np.all(spot[i] == wood_rgba)
 
 class Bot:  
 
@@ -30,57 +27,59 @@ class Bot:
             button = pag.locateCenterOnScreen('play.png')
             pag.moveTo(button)
             pag.click()
+            self.move_right()
+            self.move_right()
+            time.sleep(2.0)
             self.play()
         except:
             button = pag.locateCenterOnScreen('replay.png')
             pag.moveTo(button)
             pag.click()
+            self.move_right()
+            self.move_right()
+            time.sleep(2.0)
             self.play()
 
     # how the bot moves:
     def move_right(self):
         keyboard.press(Key.right)
         keyboard.release(Key.right)
-        time.sleep(0.16)
     def move_left(self):
         keyboard.press(Key.left)
         keyboard.release(Key.left)
-        time.sleep(0.16)
 
     #the automation itself:
     def play(self):
-        screen = mss.mss()
+
         game = {
             'top': 130,
             'left': 570,
             'width': 300,
             'height': 636
         }
-        self.move_right()
-        self.move_right()
         
         while True:
-
+            screen = mss.mss()
             img = screen.grab(game)
             img_matrix = np.array(img)
+
+            tree = [img_matrix[y, branches_x] for y in branches_y]
 
             hit = []
 
             for i in range(num_branches):
-                if is_tree(img_matrix, i):
-                    hit.append(0)
-                    hit.append(0)
-                else:
-                    hit.append(1)
-                    hit.append(1)
-
-            for h in hit:
-                if h == 0:
+                if is_tree(tree, i):
                     self.move_right()
+                    time.sleep(0.03)
+                    self.move_right()
+                    time.sleep(0.03)
                 else:
                     self.move_left()
-            
-            time.sleep(0.5)
+                    time.sleep(0.03)
+                    self.move_left()
+                    time.sleep(0.03)
+            time.sleep(1.5)
+
         
 
 if __name__ == '__main__':
